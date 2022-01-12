24afc687a14e532ed5dc8ec8a308dd2010f1ffbd
Estuardo Villeda <Estuardovilleda2001@gmail.com>
2022-01-12 16:21:05 -0600

    initial commit

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,12 @@
+import { moun } from 'marketing/MarketingApp';
+import React, { useRef, useEffect } from 'react'
+import { mount } from '../../../marketing/src/bootstrap';
+
+
+export default () => {
+    const ref = useRef(null);
+    useEffect(() => {
+        mount(ref.current)
+    })
+    return <div ref={ref} />
+}
