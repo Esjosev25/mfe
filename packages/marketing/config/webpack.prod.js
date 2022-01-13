6867479c87bc10e2375b3359eb9e33772161a84f
Estuardo Villeda <Estuardovilleda2001@gmail.com>
2022-01-12 19:17:36 -0600

    marketing fixed

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,18 +6,19 @@ const commonConfig = require('./webpack.common');
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contenhash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/marketing/latest/',
     },
     plugins: [
         new ModuleFederationPlugin({
             name: 'marketing',
             filename: 'remoteEntry.js',
             exposes: {
-                './MarketingApp': './src/bootstrap'
+                './MarketingApp': './src/bootstrap',
             },
-            shared: packageJson.dependencies
-        })
-    ]
-}
+            shared: packageJson.dependencies,
+        }),
+    ],
+};
 
-module.exports = merge(commonConfig, prodConfig)
+module.exports = merge(commonConfig, prodConfig);
