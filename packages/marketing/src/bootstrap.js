0cb4c3c3c8a9be447d320879c3e7773c94f2481f
Estuardo Villeda <Estuardovilleda2001@gmail.com>
2022-01-12 20:52:50 -0600

    fix marketing folder

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//Mount function to start up the app
-const mount = (el) => {
-    ReactDOM.render(<App />, el);
-};
-//if we r in development
-//call mount inmediately
-if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
-    if (devRoot) {
-        mount(devRoot);
-    }
-}
 
-//we are running through container
-//and we should export the mount function
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
 
 export { mount };
